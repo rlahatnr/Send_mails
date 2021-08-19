f367d74339850cafb7603d690cce017642b086ca
Lucia <rlahatnr@gmail.com>
2021-08-19 11:11:25 +0900

    add schedule code

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
+from PyQt5.QtCore import QThread
 from main_ui import Ui_MainWindow
 import sys
 import os
-import pandas as pd
+import random
 
 import smtplib
 from email import encoders
@@ -10,34 +11,116 @@ from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+import time
+
+# from apscheduler.schedulers.background import BackgroundScheduler
+#
+# def job():
+#     print('I am working...')
+#
+# sched = BackgroundScheduler()
+# sched.start()
+#
+# sched.add_job(job, 'cron', day_of_week= 3, hour= 10, minute=40, id = 'test')
+# count = 0
+# while True:
+#     time.sleep(1)
+
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.setupUi(self)
         self.setWindowTitle('이메일 대량 전송')
-        self.pushButton_3.clicked.connect(self.send_mails)
-        self.pushButton_2.clicked.connect(self.open_file)
-        self.pushButton.clicked.connect(self.get_mails)
+        self.pushButton_5.clicked.connect(self.send_mails)
+        self.pushButton_3.clicked.connect(self.open_file)
+        self.pushButton_4.clicked.connect(self.get_mails)
+
+        self.pushButton_7.clicked.connect(self.get_accounts)
+        self.pushButton_8.clicked.connect(self.send_DMs)
+
+    def send_DMs(self):
+        driver = webdriver.Chrome()
+        driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "username")))
+        uid = 'contact@crowdparti.com'
+        pwd = 'eoqkr2020$!'
+        driver.find_element_by_name('username').send_keys(uid)
+        driver_pwd = driver.find_element_by_name('password')
+        driver_pwd.send_keys(pwd)
+        driver_pwd.send_keys(Keys.ENTER)
+        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//img[@alt='Instagram']")))
+
+        insta = pd.read_excel(accounts[0])
+        for i in insta.iloc[:, 0]:
+            content = self.plainTextEdit_2.toPlainText().replace('${이름}', i)
+            try:
+                driver.get('https://www.instagram.com/direct/inbox/')
+                try:
+                    WebDriverWait(driver, 3).until(
+                        EC.element_to_be_clickable((By.XPATH, "//button[text()='나중에 하기']"))).click()
+                except:
+                    pass
+                driver.find_element_by_xpath("//button[text()='메시지 보내기']").click()
+                driver.find_element_by_xpath('//input[@placeholder="검색..."]').send_keys(i)
+                WebDriverWait(driver, 20).until(
+                    EC.element_to_be_clickable((By.XPATH, "//span[@aria-label='선택 여부 변경']")))
+                driver.find_elements_by_xpath('//span[@aria-label="선택 여부 변경"]')[0].click()
+                time.sleep(1)
+                driver.find_element_by_xpath('//div[text()="다음"]').click()
+                time.sleep(3)
+                driver.find_element_by_xpath("//input[@type='file']").send_keys(
+                    r'C:\Users\crowdparti\Downloads\크라우드파티 취급 제품 (1) (2).png')
+                for part in content.split('\n'):
+                    inputs = driver.find_element_by_xpath('//textarea[@placeholder="메시지 입력..."]')
+                    inputs.send_keys(part)
+                    inputs.send_keys("\n")
+                    time.sleep(random.random()+0.3)
+
+                print(f'{i}님께 성공적으로 DM을 전송했습니다.')
+
+            except Exception as e:
+                print(e)
+                print(f'{i}님 DM 전송에 실패했습니다.')
+                pass
+
+        msg = QMessageBox()
+        msg.setText('전송 완료하였습니다.')
+        msg.exec_()
+
+
+    def get_accounts(self):
+        global accounts
+        accounts=QFileDialog.getOpenFileName(self, 'Open File')
+        self.label_9.setText(accounts[0])
+
 
     def get_mails(self):
         global mails
         mails = QFileDialog.getOpenFileName(self, 'Open File')
-        self.label_4.setText(mails[0])
+        self.label_5.setText(mails[0])
+
 
     def open_file(self):
         global attachs
         attachs = QFileDialog.getOpenFileNames(self,'Open File')
         for i in attachs[0]:
-            exist = self.textEdit_2.toPlainText()
-            self.textEdit_2.setText(exist+i+'\n')
+            exist = self.textEdit_3.toPlainText()
+            self.textEdit_3.setText(exist+i+'\n')
 
     def send_mails(self):
+        global name
         file = pd.read_excel(mails[0])
         try:
-            for val in file.mails:
+            for name, mail in file.values:
                 send_id = 'crowdparti@naver.com'
                 send_pwd = 'eoqkr2021$!$!'
-                context = self.plainTextEdit.toPlainText()
+                context = self.plainTextEdit.toPlainText().replace('${이름}', name)
 
                 smtp = smtplib.SMTP('smtp.naver.com', 587)
                 smtp.set_debuglevel(True)
@@ -47,15 +130,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 msg = MIMEMultipart('alternative')
                 msg.set_charset('utf-8')
                 msg['From'] = send_id
-                msg['To'] = val
-                msg['Subject'] = self.lineEdit_3.text()
+                msg['To'] = mail
+                msg['Subject'] = self.lineEdit_3.text().replace('${이름}', name)
 
                 bodyPart = MIMEText(context, 'plain', 'utf-8')
                 msg.attach(bodyPart)
+                if len(self.textEdit_3.toPlainText()) > 0:
+                    print(self.textEdit_3.toPlainText())
 
-                if len(attachs) > 0:
                     attachments = list(attachs[0])
-
                     for attachment in attachments:
                         attach_binary = MIMEBase("application", "octect-stream")
                         try:
@@ -72,14 +155,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                         except Exception as e:
                             print(e)
 
-                smtp.sendmail(send_id, val, msg.as_string())
-                print('Successfully sent the mail')
+                smtp.sendmail(send_id, mail, msg.as_string())
+                time.sleep(7)
+                print(f'{name}님께 성공적으로 메일을 전송했습니다.')
         except Exception as e:
             print(e)
+            print(f'{name}님 메일 전송에 실패했습니다.')
+            time.sleep(2)
+            pass
+
         finally:
             if smtp is not None:
                 smtp.quit()
-
 
                 msg = QMessageBox()
                 msg.setText('전송 완료하였습니다.')
--- main_ui.py
+++ main_ui.py
@@ -14,62 +14,133 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(886, 657)
+        MainWindow.resize(900, 939)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.gridLayout_2 = QtWidgets.QGridLayout(self.centralwidget)
         self.gridLayout_2.setObjectName("gridLayout_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setObjectName("label_3")
-        self.gridLayout_2.addWidget(self.label_3, 3, 0, 1, 1)
-        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.centralwidget)
+        self.tabWidget = QtWidgets.QTabWidget(self.centralwidget)
+        self.tabWidget.setObjectName("tabWidget")
+        self.tab = QtWidgets.QWidget()
+        self.tab.setObjectName("tab")
+        self.gridLayout_9 = QtWidgets.QGridLayout(self.tab)
+        self.gridLayout_9.setObjectName("gridLayout_9")
+        self.plainTextEdit = QtWidgets.QPlainTextEdit(self.tab)
         self.plainTextEdit.setObjectName("plainTextEdit")
-        self.gridLayout_2.addWidget(self.plainTextEdit, 4, 0, 1, 1)
-        self.label_6 = QtWidgets.QLabel(self.centralwidget)
+        self.gridLayout_9.addWidget(self.plainTextEdit, 4, 0, 1, 1)
+        self.label_6 = QtWidgets.QLabel(self.tab)
         self.label_6.setObjectName("label_6")
-        self.gridLayout_2.addWidget(self.label_6, 1, 0, 1, 1)
-        self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_3.setObjectName("lineEdit_3")
-        self.gridLayout_2.addWidget(self.lineEdit_3, 2, 0, 1, 1)
-        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.gridLayout_9.addWidget(self.label_6, 1, 0, 1, 1)
+        self.gridLayout_3 = QtWidgets.QGridLayout()
+        self.gridLayout_3.setObjectName("gridLayout_3")
+        self.label_5 = QtWidgets.QLabel(self.tab)
+        self.label_5.setMaximumSize(QtCore.QSize(16777215, 46))
+        self.label_5.setObjectName("label_5")
+        self.gridLayout_3.addWidget(self.label_5, 3, 1, 1, 4)
+        self.pushButton_4 = QtWidgets.QPushButton(self.tab)
+        self.pushButton_4.setMaximumSize(QtCore.QSize(16777215, 46))
+        self.pushButton_4.setObjectName("pushButton_4")
+        self.gridLayout_3.addWidget(self.pushButton_4, 3, 0, 1, 1)
+        self.pushButton_3 = QtWidgets.QPushButton(self.tab)
+        self.pushButton_3.setMaximumSize(QtCore.QSize(16777215, 46))
         self.pushButton_3.setObjectName("pushButton_3")
-        self.gridLayout_2.addWidget(self.pushButton_3, 5, 0, 1, 1)
-        self.gridLayout = QtWidgets.QGridLayout()
-        self.gridLayout.setObjectName("gridLayout")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setMaximumSize(QtCore.QSize(16777215, 46))
-        self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 3, 0, 1, 1)
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setMaximumSize(QtCore.QSize(16777215, 46))
-        self.label_4.setObjectName("label_4")
-        self.gridLayout.addWidget(self.label_4, 3, 2, 1, 1)
-        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setMaximumSize(QtCore.QSize(16777215, 46))
-        self.pushButton_2.setObjectName("pushButton_2")
-        self.gridLayout.addWidget(self.pushButton_2, 0, 0, 1, 1)
-        self.textEdit_2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEdit_2.setMaximumSize(QtCore.QSize(16777215, 140))
-        self.textEdit_2.setObjectName("textEdit_2")
-        self.gridLayout.addWidget(self.textEdit_2, 1, 0, 1, 3)
-        self.gridLayout_2.addLayout(self.gridLayout, 0, 0, 1, 1)
+        self.gridLayout_3.addWidget(self.pushButton_3, 0, 0, 1, 1)
+        self.textEdit_3 = QtWidgets.QTextEdit(self.tab)
+        self.textEdit_3.setMaximumSize(QtCore.QSize(16777215, 140))
+        self.textEdit_3.setObjectName("textEdit_3")
+        self.gridLayout_3.addWidget(self.textEdit_3, 1, 0, 1, 5)
+        self.gridLayout_9.addLayout(self.gridLayout_3, 0, 0, 1, 1)
+        self.pushButton_5 = QtWidgets.QPushButton(self.tab)
+        self.pushButton_5.setObjectName("pushButton_5")
+        self.gridLayout_9.addWidget(self.pushButton_5, 5, 0, 1, 1)
+        self.lineEdit_3 = QtWidgets.QLineEdit(self.tab)
+        self.lineEdit_3.setObjectName("lineEdit_3")
+        self.gridLayout_9.addWidget(self.lineEdit_3, 2, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(self.tab)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout_9.addWidget(self.label_3, 3, 0, 1, 1)
+        self.tabWidget.addTab(self.tab, "")
+        self.tab_2 = QtWidgets.QWidget()
+        self.tab_2.setObjectName("tab_2")
+        self.gridLayout_6 = QtWidgets.QGridLayout(self.tab_2)
+        self.gridLayout_6.setObjectName("gridLayout_6")
+        self.pushButton_8 = QtWidgets.QPushButton(self.tab_2)
+        self.pushButton_8.setObjectName("pushButton_8")
+        self.gridLayout_6.addWidget(self.pushButton_8, 3, 0, 1, 1)
+        self.gridLayout_5 = QtWidgets.QGridLayout()
+        self.gridLayout_5.setObjectName("gridLayout_5")
+        self.pushButton_7 = QtWidgets.QPushButton(self.tab_2)
+        self.pushButton_7.setMaximumSize(QtCore.QSize(16777215, 46))
+        self.pushButton_7.setObjectName("pushButton_7")
+        self.gridLayout_5.addWidget(self.pushButton_7, 1, 0, 1, 1)
+        self.label_9 = QtWidgets.QLabel(self.tab_2)
+        self.label_9.setMaximumSize(QtCore.QSize(16777215, 46))
+        self.label_9.setObjectName("label_9")
+        self.gridLayout_5.addWidget(self.label_9, 1, 1, 1, 2)
+        self.gridLayout_6.addLayout(self.gridLayout_5, 0, 0, 1, 1)
+        self.label_8 = QtWidgets.QLabel(self.tab_2)
+        self.label_8.setObjectName("label_8")
+        self.gridLayout_6.addWidget(self.label_8, 1, 0, 1, 1)
+        self.plainTextEdit_2 = QtWidgets.QPlainTextEdit(self.tab_2)
+        self.plainTextEdit_2.setObjectName("plainTextEdit_2")
+        self.gridLayout_6.addWidget(self.plainTextEdit_2, 2, 0, 1, 1)
+        self.tabWidget.addTab(self.tab_2, "")
+        self.gridLayout_2.addWidget(self.tabWidget, 2, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
         self.retranslateUi(MainWindow)
+        self.tabWidget.setCurrentIndex(1)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_3.setText(_translate("MainWindow", "메일 내용"))
+        self.plainTextEdit.setPlainText(_translate("MainWindow", "${이름}님 안녕하세요.\n"
+"\n"
+"당사는 아빠스 수제 파스타 및 화덕 피자, 한상궁 시즈닝김 4종세트, 곰표 국수, 곰표 국물다시팩, 효천영농조합 장뇌삼, 해향유통 추석선물세트 등의\n"
+"다양한 브랜드 상품의 소셜 마케팅을 주관하고 있는 크라우드파티(주)입니다.\n"
+"자세한 내용은 첨부한 사진을 확인해주세요.\n"
+"\n"
+"현재 당사에서는 블로그 공구를 진행할 인플루언서를 모집하고 있습니다.\n"
+"현재 진행하려는 메인 제품은 아빠스의 수제 파스타 3종 (로제 파스타, 풍기크림 파스타, 볼로네제 파스타)입니다.\n"
+"아이들 간식 및 캠핑 밀키트 타겟이며, 진행중인 상품은 https://bit.ly/3rSidSy 쿠팡 링크에서 확인 가능합니다.\n"
+"\n"
+"아빠스 수제 파스타 외 제품 중에 공구를 진행하고 싶으신분들은 말씀해주시면 협의 가능합니다.\n"
+"(한상궁 시즈닝김 4종세트, 곰표 국수, 곰표 국물다시팩, 효천영농조합 장뇌삼, 해향유통 추석선물세트 등)\n"
+"기타 제품이 궁금하시다면 https://bit.ly/3fuTejo 당사 스마트스토어 링크를 확인해주세요.\n"
+"\n"
+"${이름}님께서 관심 있으시다면 좋은 조건으로 모시고자 합니다.\n"
+"당사와의 협업에 관심이 있으시다면,\n"
+"김진수 팀장 / 070-7761-7035(월~목 10:00~18:00) / contact@crowdparti.com로 문의주시면\n"
+"언제든지 제안서와 함께 답변 드리겠습니다. 감사합니다!⠀\n"
+"\n"
+"**실제 공구 진행은 정확한 실적 파악을 위해 자사몰을 사용할 수 있습니다."))
         self.label_6.setText(_translate("MainWindow", "메일 제목"))
-        self.pushButton_3.setText(_translate("MainWindow", "메일 전송"))
-        self.pushButton.setText(_translate("MainWindow", "이메일 목록 가져오기"))
-        self.label_4.setText(_translate("MainWindow", "파일 없음"))
-        self.pushButton_2.setText(_translate("MainWindow", "첨부파일"))
+        self.label_5.setText(_translate("MainWindow", "파일 없음"))
+        self.pushButton_4.setText(_translate("MainWindow", "이메일 목록 가져오기"))
+        self.pushButton_3.setText(_translate("MainWindow", "첨부파일"))
+        self.pushButton_5.setText(_translate("MainWindow", "메일 전송"))
+        self.lineEdit_3.setText(_translate("MainWindow", "${이름}님 안녕하세요. 공구 제안드립니다."))
+        self.label_3.setText(_translate("MainWindow", "메일 내용"))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab), _translate("MainWindow", "메일"))
+        self.pushButton_8.setText(_translate("MainWindow", "DM 전송"))
+        self.pushButton_7.setText(_translate("MainWindow", "계정 목록 가져오기"))
+        self.label_9.setText(_translate("MainWindow", "파일 없음"))
+        self.label_8.setText(_translate("MainWindow", "DM 내용"))
+        self.plainTextEdit_2.setPlainText(_translate("MainWindow", "${이름}님 안녕하세요.\n"
+"\n"
+"당사는 아빠스 수제 파스타 및 화덕 피자, 한상궁 시즈닝김 4종세트, 곰표 국수, 곰표 국물다시팩, 효천영농조합 장뇌삼, 해향유통 추석선물세트 등의 다양한 브랜드 상품의 소셜 마케팅을 주관하고 있는 크라우드파티(주)입니다. 자세한 내용은 첨부한 사진을 확인해주세요.\n"
+"\n"
+"현재 당사에서는 인스타 공구를 진행할 인플루언서를 모집하고 있습니다. 현재 진행하려는 메인 제품은 아빠스의 수제 파스타 3종 (로제 파스타, 풍기크림 파스타, 볼로네제 파스타)입니다. 아이들 간식 및 캠핑 밀키트 타겟이며, 진행중인 상품은 https://bit.ly/3rSidSy 쿠팡 링크에서 확인 가능합니다.\n"
+"\n"
+"아빠스 수제 파스타 외 제품 중에 공구를 진행하고 싶으신분들은 말씀해주시면 협의 가능합니다.(한상궁 시즈닝김 4종세트, 곰표 국수, 곰표 국물다시팩, 효천영농조합 장뇌삼, 해향유통 추석선물세트 등) 기타 제품이 궁금하시다면 https://bit.ly/3fuTejo 당사 스마트스토어 링크를 확인해주세요.\n"
+"\n"
+"${이름}님께서 관심 있으시다면 좋은 조건으로 모시고자 합니다. 당사와의 협업에 관심이 있으시다면, 김진수 팀장 / 070-7761-7035(월~목 10:00~18:00) / contact@crowdparti.com로 문의주시면 언제든지 제안서와 함께 답변 드리겠습니다. 감사합니다!⠀\n"
+"**실제 공구 진행은 정확한 실적 파악을 위해 자사몰을 사용할 수 있습니다."))
+        self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("MainWindow", "인스타DM"))
 
 
 if __name__ == "__main__":
