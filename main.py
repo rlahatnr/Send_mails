4945fc8005fd8ed9b23875d90f8a98d61f9ac2f8
rlahatnr <rlahatnr@gmail.com>
2021-12-23 14:36:19 +0900

    add requirements.txt

--- main.py
+++ main.py
@@ -34,6 +34,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.lineEdit_2.setEchoMode(self.lineEdit_2.Password)
         self.lineEdit_7.setEchoMode(self.lineEdit_7.Password)
 
+    # 인스타 dm 보내는 코드
     def send_DMs(self):
         chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
         try:
@@ -43,6 +44,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             driver = webdriver.Chrome(f'./{chrome_ver}/chromedriver.exe')
         driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.NAME, "username")))
+
+        # gui 상의 아이디와 비밀번호를 받는다.
         uid = self.lineEdit_6.text()
         pwd = self.lineEdit_7.text()
         driver.find_element_by_name('username').send_keys(uid)
@@ -51,6 +54,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         driver_pwd.send_keys(Keys.ENTER)
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//img[@alt='Instagram']")))
 
+        # 첨부한 계정 목록 파일 [인스타그램 id]에 따라 계정을 검색하고 gui 상의 dm내용을 전송함
+        # 해당 아이디가 없다면 (수정 등의 이유로) 가장 비슷한 아이디를 찾아 전송한다.
         insta = pd.read_excel(accounts[0])
         for i in insta.iloc[:, 0]:
             content = self.plainTextEdit_2.toPlainText().replace('${이름}', i)
@@ -69,7 +74,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 time.sleep(1)
                 driver.find_element_by_xpath('//div[text()="다음"]').click()
                 time.sleep(3)
-                # 첨부파일 넣기
+                '''
+                사진 첨부 코드: 아래의 코드를 활성화 하면 사진 첨부 가능하지만 
+                묶어보내기 기능이 없으므로 여러 사진을 보내야한다면 아래 코드를 for문으로 돌린다.
+                '''
                 # driver.find_element_by_xpath("//input[@type='file']").send_keys(
                 #     r'첨부파일/경로/파일명.png')
                 for part in content.split('\n'):
@@ -90,34 +98,19 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         msg.exec_()
 
 
-    def get_accounts(self):
-        global accounts
-        accounts=QFileDialog.getOpenFileName(self, 'Open File')
-        self.label_9.setText(accounts[0])
-
-
-    def get_mails(self):
-        global mails
-        mails = QFileDialog.getOpenFileName(self, 'Open File')
-        self.label_5.setText(mails[0])
-
-
-    def open_file(self):
-        global attachs
-        attachs = QFileDialog.getOpenFileNames(self,'Open File')
-        for i in attachs[0]:
-            exist = self.textEdit_3.toPlainText()
-            self.textEdit_3.setText(exist+i+'\n')
-
+    # 메일을 전송하는 코드
     def send_mails(self):
         global name
         file = pd.read_excel(mails[0])
         try:
+            # 첨부파일에서 아이디(닉네임)과 메일 가져옴
             for name, mail in file.values:
+                # gui상 아이디와 비밀번호, 메일 내용 가져옴
                 send_id = self.lineEdit.text()
                 send_pwd = self.lineEdit_2.text()
                 context = self.plainTextEdit.toPlainText()
 
+                # 메일 대량전송 코드(구글의 경우 보안이 심해 네이버로 작성)
                 smtp = smtplib.SMTP('smtp.naver.com', 587)
                 smtp.set_debuglevel(True)
                 smtp.starttls()
@@ -159,7 +152,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             print(f'{name}님 메일 전송에 실패했습니다.')
             time.sleep(2)
             pass
-
+        
+        # 여러건 모두 전송이 끝나면 종료
         finally:
             if smtp is not None:
                 smtp.quit()
@@ -167,6 +161,26 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 msg = QMessageBox()
                 msg.setText('전송 완료하였습니다.')
                 msg.exec_()
+
+    # 첨부파일에서 인스타 계정 가져옴
+    def get_accounts(self):
+        global accounts
+        accounts=QFileDialog.getOpenFileName(self, 'Open File')
+        self.label_9.setText(accounts[0])
+
+    # 첨부파일에서 이메일을 가져옴
+    def get_mails(self):
+        global mails
+        mails = QFileDialog.getOpenFileName(self, 'Open File')
+        self.label_5.setText(mails[0])
+
+    # 첨부파일을 첨부하는 코드
+    def open_file(self):
+        global attachs
+        attachs = QFileDialog.getOpenFileNames(self,'Open File')
+        for i in attachs[0]:
+            exist = self.textEdit_3.toPlainText()
+            self.textEdit_3.setText(exist+i+'\n')
 
 if __name__=='__main__':
     app = QApplication(sys.argv)
